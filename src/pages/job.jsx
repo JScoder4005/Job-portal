6b77941ae5b98d70aa11f5a964dd2bb1bf738dfb
Uday kumar M <136188719+JScoder4005@users.noreply.github.com>
2024-10-21 15:25:58 +0530

    updated job page

--- src/pages/job.jsx
+++ src/pages/job.jsx
@@ -1,5 +1,108 @@
+import { getSingleJob, updateHiringStatus } from '@/api/apiJobs';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import useFetch from '@/hooks/use-fetch';
+import { useUser } from '@clerk/clerk-react';
+import MDEditor from '@uiw/react-md-editor';
+import { Briefcase, DoorClosed, DoorOpen, MapPinIcon } from 'lucide-react';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { BarLoader } from 'react-spinners';
+
 const JobPage = () => {
-  return <div></div>;
+  const { isLoaded, user } = useUser();
+  const { id } = useParams();
+
+  const {
+    loading: loadingJob,
+    data: job,
+    fn: fnjob,
+  } = useFetch(getSingleJob, { job_id: id });
+
+  const { loading: loadingHiringStatus, fn: fnHiringStatus } = useFetch(
+    updateHiringStatus,
+    { job_id: id }
+  );
+
+  const handleStatusChange = (value) => {
+    const isOpen = value === 'Open';
+    fnHiringStatus(isOpen).then(() => fnjob());
+  };
+
+  useEffect(() => {
+    if (isLoaded) fnjob();
+  }, [isLoaded]);
+
+  if (!isLoaded || loadingJob) {
+    return <BarLoader className="mb-4" width={'100%'} color="36d7b7" />;
+  }
+
+  return (
+    <div className="flex flex-col gap-8 mt-5">
+      <div className="flex flex-col-reverse gap-6 md:flex-row justify-between items-center">
+        <h1 className="gradient-title font-extrabold pb-3 text-4xl sm:text-6xl">
+          {job?.title}
+        </h1>
+        <img src={job?.company?.logo_url} className="h-12" alt={job?.title} />
+      </div>
+
+      <div className="flex justify-between">
+        <div className="gap-2 flex">
+          <MapPinIcon />
+          {job?.location}
+        </div>
+        <div className="flex gap-2">
+          <Briefcase />
+          {job?.applications.length}
+        </div>
+        <div className="flex gap-2">
+          {job?.isOpen ? (
+            <>
+              <DoorOpen /> Open
+            </>
+          ) : (
+            <>
+              <DoorClosed /> Closed
+            </>
+          )}
+        </div>
+      </div>
+      {/* hiring status */}
+      {!loadingHiringStatus && <BarLoader width={'100%'} color="36d7b7" />}
+      {job?.recruiter_id === user?.id && (
+        <Select onValueChange={handleStatusChange}>
+          <SelectTrigger
+            className={`w-full ${job?.isOpen ? 'bg-green-950' : 'bg-red-950'}`}
+          >
+            <SelectValue
+              placeholder={
+                'Hiring Status ' + (job?.isOpen ? '( Open )' : '( Closed )')
+              }
+            />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="open">Open</SelectItem>
+            <SelectItem value="closed">Closed</SelectItem>
+          </SelectContent>
+        </Select>
+      )}
+      <h2 className="text-2xl sm:text-3xl font-bold">About the job</h2>
+      <p className="sm:text-lg">{job?.description}</p>
+
+      <h2 className="text-2xl sm:text-3xl font-bold">
+        What we are looking for?{' '}
+      </h2>
+      <MDEditor.Markdown
+        source={job?.requirements}
+        className="bg-transparent sm:text-lg"
+      />
+    </div>
+  );
 };
 
 export default JobPage;
